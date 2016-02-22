009ff9dedca06c72c1be9b45fd037779f7dc48d5
Kevin Smith <thirdwiggin@gmail.com>
2016-02-21 19:01:44 -0600

    Interrups: add vector numbers

--- interrupts.h
+++ interrupts.h
@@ -3,9 +3,30 @@
 
 #include <stdint.h>
 
-#define VECTOR_COUNT                    48
-
 typedef uint8_t vector_t;
+enum {
+    VECTOR_NMI                          = 3,
+    VECTOR_HARD_FAULT                   = 4,
+    VECTOR_SVCALL                       = 11,
+    VECTOR_PENDABLE_SRV_REQ             = 14,
+    VECTOR_SYSTICK                      = 15,
+    VECTOR_FTFA                         = 21,
+    VECTOR_PMC                          = 22,
+    VECTOR_LLWU                         = 23,
+    VECTOR_I2C0                         = 24,
+    VECTOR_SPI0                         = 26,
+    VECTOR_LPUART0                      = 28,
+    VECTOR_ADC0                         = 31,
+    VECTOR_CMP0                         = 32,
+    VECTOR_TPM0                         = 33,
+    VECTOR_TPM1                         = 34,
+    VECTOR_RTC_ALARM                    = 36,
+    VECTOR_RTC_SECONDS                  = 37,
+    VECTOR_LPTMR0                       = 44,
+    VECTOR_PORT_A                       = 46,
+    VECTOR_PORT_B                       = 47,
+    VECTOR_COUNT
+};
 
 typedef void (*isr_t)(void);
 
